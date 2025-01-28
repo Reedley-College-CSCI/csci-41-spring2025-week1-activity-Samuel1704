05bc2b447ac9e5df8d193f79623eafc10ea20673
Samuel Alves <samuelmontana@aol.com>
2025-01-27 19:45:28 -0800

    I added a thrid simplevector

--- SimpleVectorDemo.cpp
+++ SimpleVectorDemo.cpp
@@ -11,12 +11,16 @@ int main()
     // Create a SimpleVector of ints.
     SimpleVector<int> intTable(SIZE);
 
+    // Create an additional SimpleVector of ints.
+    SimpleVector<int> intTable2(SIZE);
+
     // Create a SimpleVector of doubles.
     SimpleVector<double> doubleTable(SIZE);
 
     // Store values in the two SimpleVectors.
     for (count = 0; count < SIZE; count++)
     {
+        intTable2[count]=(count*4);//Added a multiplier section for intTable2
         intTable[count] = (count * 2);
         doubleTable[count] = (count * 2.14);
     }
@@ -25,6 +29,10 @@ int main()
     cout << "These values are in intTable:\n";
     for (count = 0; count < SIZE; count++)
         cout << intTable[count] << " ";
+    cout << endl;
+    cout << "These values are in intTable2:\n";//new display value section for intTable2
+    for (count = 0; count < SIZE; count++)
+        cout << intTable2[count] << " ";
     cout << endl;
     cout << "These values are in doubleTable:\n";
     for (count = 0; count < SIZE; count++)
@@ -37,6 +45,7 @@ int main()
     for (count = 0; count < SIZE; count++)
     {
         intTable[count] = intTable[count] + 5;
+        intTable2[count] = intTable2[count] + 5;//added and addition section for intTable2
         doubleTable[count] = doubleTable[count] + 5.0;
     }
 
@@ -44,6 +53,10 @@ int main()
     cout << "These values are in intTable:\n";
     for (count = 0; count < SIZE; count++)
         cout << intTable[count] << " ";
+    cout << endl;
+     cout << "These values are in intTable2:\n";//new display value section for intTable2
+    for (count = 0; count < SIZE; count++)
+        cout << intTable2[count] << " ";
     cout << endl;
     cout << "These values are in doubleTable:\n";
     for (count = 0; count < SIZE; count++)
@@ -56,6 +69,7 @@ int main()
     for (count = 0; count < SIZE; count++)
     {
         intTable[count]++;
+        intTable2[count]++;//added a ++ section for intTable2
         doubleTable[count]++;
     }
 
@@ -63,6 +77,10 @@ int main()
     cout << "These values are in intTable:\n";
     for (count = 0; count < SIZE; count++)
         cout << intTable[count] << " ";
+    cout << endl;
+     cout << "These values are in intTable2:\n";//new display value section for intTable2
+    for (count = 0; count < SIZE; count++)
+        cout << intTable2[count] << " ";
     cout << endl;
     cout << "These values are in doubleTable:\n";
     for (count = 0; count < SIZE; count++)
